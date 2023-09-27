014f7ae325f35a8cea5b975b82640694fdee3619
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-27 13:39:24 +0530

    * Added app .

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,50 @@
-import { memo } from "preact/compat"
-import BreakingLogo from './components/BreakingLogo'
-import { useState,useEffect } from "preact/hooks"
-import Breakify from './helpers/Breakify'
-const App=()=> {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [first, setFirst] = useState(['', '', ''])
-  const [last, setLast] = useState(['', '', ''])
+import { memo } from "preact/compat";
+import BreakingLogo from "./components/BreakingLogo";
+import { useState, useEffect } from "preact/hooks";
+import Breakify from "./helpers/Breakify";
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [first, setFirst] = useState(["", "", ""]);
+  const [last, setLast] = useState(["", "", ""]);
 
   useEffect(() => {
-    setFirst(Breakify(firstName))
-  }, [firstName])
+    setFirst(Breakify(firstName));
+  }, [firstName]);
 
   useEffect(() => {
-    setLast(Breakify(lastName))
-  }, [lastName])
+    setLast(Breakify(lastName));
+  }, [lastName]);
 
+  const handleFirstNameChange = (e: Event) => {
+    if (e.target instanceof HTMLInputElement) {
+      setFirstName(e.target.value);
+    }
+  };
+
+  const handleLastNameChange = (e: Event) => {
+    if (e.target instanceof HTMLInputElement) {
+      setLastName(e.target.value);
+    }
+  };
   return (
-    <div className='app'>
-      <div className='content'>
+    <div className="app">
+      <div className="content">
         <BreakingLogo result={first} />
         <BreakingLogo result={last} />
-        <div className='row'>
-          <div className='col'>
+        <div className="row">
+          <div className="col">
             <label>First Name</label>
-            <input onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+            <input onChange={handleFirstNameChange} value={firstName} />
           </div>
-          <div className='col'>
+          <div className="col">
             <label>Last Name</label>
-            <input onChange={(e) => setLastName(e.target.value)} value={lastName} />
+            <input onChange={handleLastNameChange} value={lastName} />
           </div>
         </div>
         <button>Breakify</button>
       </div>
     </div>
-  )
-}
-export default memo(App)
+  );
+};
+export default memo(App);
